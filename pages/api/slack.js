1d2f9e5c49f4fca22bd24477c03c73df56e3807c
Fawwaz Muhammad <fawwazmuhammad@gmail.com>
2020-04-06 16:37:38 +0700

    troll afrishal more

--- pages/api/slack.js
+++ pages/api/slack.js
@@ -17,7 +17,7 @@ function runMiddleware(req, res, fn) {
 function getMessage(array) {
   const time = Math.floor(getDate(new Date()) / 1000) + 3;
   const selectedIdx = time % array.length;
-  return array[0];
+  return array[1];
 }
 
 const cors = new Cors({
@@ -30,6 +30,7 @@ const slack = new WebClient(token);
 
 const messages = [
   'Hi @accom-web-eng daily slackup time ! Please log your activities in this thread',
+  'Hi @accom-web-eng sudah hampir 3 tahun gw jadi lead kalian, gw minta maaf sebesar-besarnya kalau ada yang salah. Mudah mudahan kita bisa ketemu lagi setelah corona padam. dengan breat hati gw sampaikan gw ulang tahun hari ini. TOLONG DONG DIUCAPIN SELAMAT DONG!! Kalau mau traktir gw bisa kirim gofood ke alamat ini : Bassura City Apartment Tower Edelweiss, Unit E/17/BG, JI. Jend. Basuki Rachmat Blok Al Abror No. 45 RT8/RW10 Jatinegara, Jakarta Timur, 13410 DKI Jakarta ditunggu loh. jangan lupa laporan hari ini ngapain aja di thread ini guys !!',
   'Sebagai bos, saya mau minta laporan kalian hari ini ngapain aja. KERJA WOI !!! @accom-web-eng, pecut nih !',
   'Ayo @accom-web-eng, ngapain aja hari ini ?',
   'Gw udah masuk kantor kok kalian gak ada ? coba seharian ini kalian ngapaain aja ? @accom-web-eng',
